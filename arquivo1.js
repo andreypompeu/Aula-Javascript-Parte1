75ae2419f107247abcdf4cbcab542c4695683393
andreypompeu <andreypompeu@outlook.com>
2024-04-23 03:17:42 -0300

    Subindo aula de JavaScript

---
+++ arquivo1.js
@@ -0,0 +1,44 @@
+
+const abacate = 40
+let Andrey = "Aluno muito aplicado"
+let Pompeu = 30
+
+
+console.log(abacate)
+
+console.log(Andrey)
+
+console.log(Pompeu)
+
+alert("Apenas testando")
+
+// Objeto //
+
+const rodolfo = {
+    name: "Rodolfo",
+    age: 27,
+    address: "Rua dos bobos"
+
+}
+
+console.log(rodolfo)
+
+// Array //
+
+const testandoArray = ["Andrey", 27, "Fortaleza Ceará"]
+
+testandoArray[1] = 28
+
+console.log(testandoArray)
+
+// IF E ELSE //
+
+const notaDoAluno = 3
+const notaDeCorte = 5
+
+if (notaDoAluno > notaDeCorte) {
+    console.log("Parabéns você foi aprovado")
+} 
+    else {
+        console.log("Você foi reprovado")
+    }
